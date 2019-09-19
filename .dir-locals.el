89d63593d42a5b6de640a577792ea400d4af225e
Ben Simms <ben@bensimms.moe>
2019-09-19 16:09:00 +0100

    We don't want that

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,4 @@
          (ssh-deploy-root-local . "~/dev/netbricks_test/")
          (ssh-deploy-root-remote . "/ssh:zizek_p:netbricks_test/")
          (ssh-deploy-async . 1)
-         (ssh-deploy-on-explicit-save . 1)
          )))
